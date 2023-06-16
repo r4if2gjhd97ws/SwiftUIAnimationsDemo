0cb408d182babe80c42f009ecc4ba29876d6c4f1
yohei <yohei@yoheinoMacBook-Pro-2023.local>
2023-06-16 12:00:58 +0900

    button A を一回押下するとbutton Bが真円からpill型に変化する

---
+++ SwiftUIAnimationsDemo/SwiftUIAnimationsDemo/ButtonCircleToPill.swift
@@ -0,0 +1,74 @@
+//
+//  ButtonCircleToPill.swift
+//  SwiftUIAnimationsDemo
+//
+//  Created by yohei on 2023/06/16.
+//
+
+import SwiftUI
+
+
+// button A を一回押下するとbutton Bが真円からpill型に変化する
+struct ButtonCircleToPill: View {
+  @State private var isExpanded = false
+
+
+  var body: some View {
+    HStack {
+
+      HStack {
+        Spacer()
+
+        Text("x")
+          .frame(width: 78)
+        Spacer()
+      }
+        .background(.yellow)
+
+      HStack {
+        Button(action: {
+          withAnimation {
+            self.isExpanded.toggle()
+          }
+        }) {
+          Text("A")
+        }
+          .padding()
+          .frame(width: 71, height: 71)
+
+      }
+        .frame(alignment: .center)
+        .background(.red)
+
+      HStack {
+        Spacer()
+          .frame(width: isExpanded ? 50 : 96)
+        Button(action: { }) {
+          Text("B")
+        }
+          .padding()
+          .frame(width: isExpanded ? 96 : 50, height: 50)
+          .background(isExpanded ? .blue : .gray)
+          .foregroundColor(.white)
+          .clipShape(Capsule())
+        Spacer()
+      }
+        .animation(.default, value: isExpanded)
+
+    }
+      .background()
+      .frame(width: UIScreen.main.bounds.width - 20)
+      .padding(.horizontal, 10)
+
+  }
+}
+
+struct ButtonCircleToPill_Previews: PreviewProvider {
+  static var previews: some View {
+    ZStack {
+      Color(.gray)
+
+      ButtonCircleToPill()
+    }
+  }
+}
