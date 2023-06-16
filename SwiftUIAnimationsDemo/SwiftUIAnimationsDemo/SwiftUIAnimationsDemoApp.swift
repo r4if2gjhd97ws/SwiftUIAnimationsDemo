03331a6ff0a0d6e4fcf6b7b40e2d19538a6a96a8
yohei <yohei@yoheinoMacBook-Pro-2023.local>
2023-06-16 11:20:59 +0900

    project追加

---
+++ SwiftUIAnimationsDemo/SwiftUIAnimationsDemo/SwiftUIAnimationsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsDemoApp.swift
+//  SwiftUIAnimationsDemo
+//
+//  Created by yohei on 2023/06/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIAnimationsDemo/SwiftUIAnimationsDemoUITests/SwiftUIAnimationsDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUIAnimationsDemoUITestsLaunchTests.swift
+//  SwiftUIAnimationsDemoUITests
+//
+//  Created by yohei on 2023/06/16.
+//
+
+import XCTest
+
+final class SwiftUIAnimationsDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
